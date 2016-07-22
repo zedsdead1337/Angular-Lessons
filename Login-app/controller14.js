67c962f3208105f942f3b3cd514319d41367b50a
zedsdead1337 <1337kryptic@gmail.com>
2016-07-22 10:11:59 +0530

    Add files via upload

---
+++ Login-app/controller14.js
@@ -0,0 +1,32 @@
+var app = angular.module('mainApp',['ngRoute']);
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl:'14login.html' 
+	})
+	.when('/dashboard',{
+		resolve:{
+			"check": function($location,$rootScope){
+				if(!rootScope.loggedIn){
+					$location.path('/');
+				}
+			}
+		},
+		templateUrl:'dashboard.html'
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
+
+});
+
+app.controller('loginCtrl',function($scope,$location,$rootScope){
+	$scope.submit =  function(){
+
+		if($scope.username=='admin' && $scope.password=='admin'){
+			$rootScope.loggedIn=true;
+			$location.path('/dashboard');
+			//or we can also use window.location.hash = '#/dashboard';
+		}
+	}
+});
